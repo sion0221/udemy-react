fa1eb9f9408b7f2bfcd61072bd8e491cf53c330d
Heosion <tldhs123e@gmail.com>
2026-01-22 23:48:37 +0900

    learn : 상태변경에 따른 입력값 출력 - 입력하고 버튼 클릭 시, 이름이 화면에 렌더링 - 만약, 인풋값을 지울 땐 false에 따른 값이 출력

--- 05-player-project/src/components/Player.jsx
+++ 05-player-project/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enterPlayerName, setEneterPlayerName] = useState(null);
+
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEneterPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
