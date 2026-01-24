c7e180f0ff7c756ad3743a3b9bbab6496b87c136
Heosion <tldhs123e@gmail.com>
2026-01-24 20:45:57 +0900

    learn : 타이머 시작 버튼 제작

--- 05-player-project/src/components/TimerChallenge.jsx
+++ 05-player-project/src/components/TimerChallenge.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
-        {targetTime} {targetTime > 1 ? "초" : ""}
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>챌린지 시작</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} 챌린지
+        </button>
       </p>
-      <p className="">시간이 가는 중... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "시간이 가는 중..." : "시간 비활성화"}
+      </p>
     </section>
   );
 }
