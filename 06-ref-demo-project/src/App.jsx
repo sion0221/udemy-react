af7df87db47c2301a2e2535a82ccc94040432031
Heosion <tldhs123e@gmail.com>
2026-01-27 16:57:02 +0900

    learn : Ref(참조)와 전달된 Refs(참조)로 사용자 입력 받아오기

--- 06-ref-demo-project/src/App.jsx
+++ 06-ref-demo-project/src/App.jsx
@@ -5,17 +5,17 @@ import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
-  const [projectState, setProjectState] = useState({
+  const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     // 정의되지 않은 값으로 준 이유?
     // selectedProject 속성을 사용해서 나중에 프로젝트가 여러 개 있을 때,
     // null 값 : 새로운 프로젝트를 추가하고 싶을 때 사용
     // undefined 값 : 추가한 새 프로젝트가 없거나 아무 프로젝트도 선택하지 않았을 때 사용
-    project: [],
+    projects: [],
   });
 
   function handleStartAddProject() {
-    setProjectState((prev) => {
+    setProjectsState((prev) => {
       return {
         ...prev,
         selectedProjectId: null,
@@ -23,11 +23,27 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prev) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prev,
+        projects: [...prev.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
-  if (projectState.selectedProjectId === null) {
-    content = <NewProject />;
-  } else if (projectState.selectedProjectId === undefined) {
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
--- 06-ref-demo-project/src/components/NewProject.jsx
+++ 06-ref-demo-project/src/components/NewProject.jsx
@@ -1,6 +1,25 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enterTitle = title.current.value;
+    const enterDescription = description.current.value;
+    const enterDueDate = dueDate.current.value;
+
+    // validation(검증)
+
+    onAdd({
+      title: enterTitle,
+      description: enterDescription,
+      dueDate: enterDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,15 +27,18 @@ export default function NewProject() {
           <button className="text-stone-800 hover:text-stone-950">취소</button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             저장
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due DAte" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due DAte" />
       </div>
     </div>
   );
