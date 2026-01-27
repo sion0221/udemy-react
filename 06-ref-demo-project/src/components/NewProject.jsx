8c22786a3c6d24ef308b308fd757ae13a983f791
Heosion <tldhs123e@gmail.com>
2026-01-27 17:23:05 +0900

    learn : 사용자 입력 유효성 검사 & useImperativeHandle로 에러 모달 띄우기

--- 06-ref-demo-project/src/components/NewProject.jsx
+++ 06-ref-demo-project/src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +14,14 @@ export default function NewProject({ onAdd }) {
     const enterDescription = description.current.value;
     const enterDueDate = dueDate.current.value;
 
-    // validation(검증)
+    if (
+      enterTitle.trim() === "" ||
+      enterDescription.trim() === "" ||
+      enterDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enterTitle,
@@ -21,25 +31,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">취소</button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            저장
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due DAte" />
+    <>
+      <Modal ref={modal} buttonCaption="닫기">
+        <h2>입력 오류</h2>
+        <p>Oops... 값을 입력하지 않은 것 같습니다</p>
+        <p>유효한 값을 입력해주세요!</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              취소
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              저장
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due DAte" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
